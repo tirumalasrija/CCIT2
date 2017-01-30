fd4ca708b27b361182ea07217d89577f4ab0f2aa
Sai Haridas <sharidass1408@gmail.com>
2017-01-29 21:31:39 -0500

    Initial commit for CCIT

---
+++ js/redirect.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+
+
+$("a[href='#Strategy']").click(function(){
+	window.location.href = 'index.html#Strategy';
+});
+
+$("a[href='#solution']").click(function(){
+	window.location.href = 'index.html#solution';
+});
+
+$("a[href='#data']").click(function(){
+	window.location.href = 'index.html#data';
+});
+
+$("a[href='#Statistics']").click(function(){
+	window.location.href = 'index.html#Statistics';
+});
+
+$("a[href='#communication']").click(function(){
+	window.location.href = 'index.html#communication';
+});
